0f960429984edf0922bd6f3e234418b1ca790584
Felix670-dot <felixperez233@gmail.com>
2025-04-30 22:01:37 -0400

    Update PlatformerGame.java

--- src/PlatformerGame.java
+++ src/PlatformerGame.java
@@ -4,10 +4,12 @@ public class PlatformerGame extends GameBase {
     final double GRAVITY = 1.1;
 
     Player player = new Player(200, 200);
-    TileMap map = new TileMap("map1.txt" , 64);
+    TileMap map;
 
     public void initialize() {
         player.setAcceleration(GRAVITY);
+        map = new TileMap("map1.txt" , 64);
+        map = new TileMap("map2.txt" , 64);
     }
 
     public void inGameLoop() {
@@ -48,11 +50,23 @@ public class PlatformerGame extends GameBase {
                 }
             }
         }
+        //to enter map to the left
+        if( player.x < (TileMap.maps[TileMap.current].getLeftLimit()) && (TileMap.current != 0)) {
+        	map.changeMap(TileMap.current - 1);
+        	player.x = TileMap.maps[TileMap.current].getRightLimit();
+        	Camera.x = TileMap.maps[TileMap.current].getRightLimit() - 1920;
+        }
+        //to enter the room to the right
+        if( player.x > (TileMap.maps[TileMap.current].getRightLimit()) && (TileMap.current != 1)) {
+        	map.changeMap(TileMap.current + 1);
+        	player.x = TileMap.maps[TileMap.current].getLeftLimit();
+        	Camera.x = TileMap.maps[TileMap.current].getLeftLimit();
+        }
     }
 
     public void paint(Graphics pen) {
         player.draw(pen);
-
+        TileMap.maps[TileMap.current].draw(pen);
         Rect[] bounds = TileMap.maps[TileMap.current].getBounds();
         for (Rect bound : bounds) {
             pen.drawRect((int)bound.x, (int)bound.y, bound.w, bound.h);
